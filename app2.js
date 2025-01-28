71e9fdfb0458f638a9f4a5170858457c0aac90cd
Your Name <you@example.com>
2025-01-28 12:50:11 +0500

    init

---
+++ app2.js
@@ -0,0 +1,10 @@
+VANTA.NET({
+    el: "#sections",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 500.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00
+  })
